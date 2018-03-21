e15dc979557a1a4c126673443e5c582bec90c332
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2018-03-21 10:57:44 +0000

    Simple eval operations on button click.

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ var calc = {
 	clear: function() {
 		this._setDisplay('');
 	},
+	decimal: function() {
+		this.result = eval(this.elements.display.value + '/10');
+		this._setDisplay(this.result);
+	},
 
 	del: function() {
 		var displayValue =  this.elements.display.value.slice(0,-1);
@@ -22,9 +26,19 @@ var calc = {
 		this._setDisplay(displayValue);
 	},
 	equals: function() {
+		//this.result = this.elements.display.value;
+		//var evalCode = ''+ (this.result / 100) +';';
 		this.result = this.elements.display.value;
-		var evalCode = ''+ (this.result / 100) +';';
-		
-		this._setDisplay(eval(evalCode));
+		this._setDisplay(eval(this.result));
 	}
 };
+
+var $numericPad = document.querySelector('.buttons-container');
+var $display = document.getElementById('display');
+
+$numericPad.addEventListener('click', function(event){
+	var $target = event.target;
+	if($target.dataset.value) {
+		$display.value +=  $target.dataset.value;
+	}
+});
